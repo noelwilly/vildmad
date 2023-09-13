b9cce6712639fe940d15d9e08237fe5bf7395c0e
NoelWilliam <noelwilliamm@gmail.com>
2023-09-13 13:57:43 +0200

    js

--- skov.js
+++ skov.js
@@ -1,27 +1,68 @@
-/* const url = "https://pexwboaieovroxjkczsc.supabase.co";
+const url = "https://pexwboaieovroxjkczsc.supabase.co";
 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBleHdib2FpZW92cm94amtjenNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1MDEwNzgsImV4cCI6MjAxMDA3NzA3OH0.6BHSqoC3DM7BXM7_X4Syq5W3u_U5MKO7EPClIkefExs";
 
-fetch(url + "/rest/v1/vildapi?select=sankeland", {
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("sankeland");
+
+fetch(url + "/rest/v1/vildapi?sankeland=eq." + category, {
   method: "GET",
   headers: {
     apikey: key,
   },
 })
   .then((res) => res.json())
-  .then(showSkovs);
+  .then(showProducts);
 
-function showSkovs(skovs) {
-  skovs.forEach(showSkov);
+function showProducts(products) {
+  //LOOPER OG KALDER showProduct
+  products.forEach(showProduct);
 }
 
-function showSkov(skov) {
-  // fanger template
-  const template = document.querySelector("template").content;
-  // kloner
-  const clone = template.cloneNode(true);
+function showProduct(product) {
+  console.log(product);
+  // fang template
+  const template = document.querySelector("#vmProduktTemplate").content;
+  // lav en kopi
+  const copy = template.cloneNode(true);
   // ændre indhold
-  clone.querySelector("a").href = `produktside.html?sankeland=${skov.sankeland}`;
-  // appender
-  document.querySelector(".category-container").appendChild(clone);
-} */
+  copy.querySelector("h3").textContent = product.navn;
+  copy.querySelector("h4").textContent = product.sankeland;
+  copy.querySelector(".kategori").textContent = product.kategori;
+  copy.querySelector(".vmPic").src = product.billeder;
+  copy.querySelector(".lokationApi").textContent = product.subkategori;
+
+  // seasons
+  if (product.season.includes("Forår")) {
+    copy.querySelector(".season1").classList.add("seasonOn");
+  }
+  if (product.season.includes("Sommer")) {
+    copy.querySelector(".season2").classList.add("seasonOn");
+  }
+  if (product.season.includes("Efterår")) {
+    copy.querySelector(".season3").classList.add("seasonOn");
+  }
+
+  // appende
+  document.querySelector(".produktliste").appendChild(copy);
+}
+
+/*
+billeder: "https://vildmadv2.vps.webdock.io/application/files/8916/2436/4262/Kantarel_ravarekort_app.png"​​
+id: 278
+kategori: "Svampe"
+navn: "Kantarel, almindelig"
+sankeland: "Løvskov"
+season: '["Sommer", "Efterår"]'
+subkategori: "Morbund" */
+
+/* function showData(items) {
+    console.log(items);
+    items.forEach((element) => {
+      //tjek om der er noget i kolonnen
+      if (element.tags) {
+        // hvis der er noget i kolonnen skal objekterne der indeholder "vand" vises
+        if (element.tags.includes("forår")) console.log(element);
+      }
+    });
+  } */
