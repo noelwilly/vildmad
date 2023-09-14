7db7b45c2bca6930d7f7ee84494b3e88ca586fc8
NoelWilliam <noelwilliamm@gmail.com>
2023-09-14 09:14:11 +0200

    JS virker nu

--- produkt.js
+++ produkt.js
@@ -1,45 +1,39 @@
 const url = "https://pexwboaieovroxjkczsc.supabase.co";
 
-const key =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBleHdib2FpZW92cm94amtjenNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1MDEwNzgsImV4cCI6MjAxMDA3NzA3OH0.6BHSqoC3DM7BXM7_X4Syq5W3u_U5MKO7EPClIkefExs";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBleHdib2FpZW92cm94amtjenNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1MDEwNzgsImV4cCI6MjAxMDA3NzA3OH0.6BHSqoC3DM7BXM7_X4Syq5W3u_U5MKO7EPClIkefExs";
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-fetch(
-  url +
-    "/rest/v1/vildapi?select=navn,season,billeder,kategori,sankeland,subkategori&id=eq." +
-    id,
-  {
-    method: "GET",
-    headers: {
-      apikey: key,
-    },
-  }
-)
+fetch(url + "/rest/v1/vildapi?select=navn,season,billeder,kategori,sankeland,subkategori&id=eq." + id, {
+  method: "GET",
+  headers: {
+    apikey: key,
+  },
+})
   .then((response) => response.json())
   .then((data) => showProduct(data));
 
 function showProduct(product) {
   console.log(product);
 
-  document.querySelector("h3").textContent = product.navn;
+  document.querySelector("h3").textContent = product[0].navn;
 
-  document.querySelector(".vmPic").src = product.billeder;
+  document.querySelector(".vmPic").src = product[0].billeder;
 
-  document.querySelector("h4").textContent = product.sankeland;
-  document.querySelector(".kategori").textContent = product.kategori;
+  document.querySelector("h4").textContent = product[0].sankeland;
+  document.querySelector(".kategori").textContent = product[0].kategori;
 
-  document.querySelector(".lokationApi").textContent = product.subkategori;
+  document.querySelector(".lokationApi").textContent = product[0].subkategori;
 
   // seasons
-  if (product.season.includes("Forår")) {
+  if (product[0].season.includes("Forår")) {
     document.querySelector(".season1").classList.add("seasonOn");
   }
-  if (product.season.includes("Sommer")) {
+  if (product[0].season.includes("Sommer")) {
     document.querySelector(".season2").classList.add("seasonOn");
   }
-  if (product.season.includes("Efterår")) {
+  if (product[0].season.includes("Efterår")) {
     document.querySelector(".season3").classList.add("seasonOn");
   }
 }
